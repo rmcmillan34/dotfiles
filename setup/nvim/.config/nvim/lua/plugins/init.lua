5a3545e61451c72df77b6df39e232fd9106d5203
Ryan McMillan <Ryan.j.mcmillan34@gmail.com>
2026-02-14 17:57:28 +1100

    Initialize lazy.nvim plugin manager

---
+++ setup/nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,23 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { import = "plugins.files" },
+  { import = "plugins.git" },
+  { import = "plugins.lsp" },
+  { import = "plugins.treesitter" },
+  { import = "plugins.format" },
+  { import = "plugins.markdown" },
+  { import = "plugins.latex" },
+}, {
+  ui = { border = "rounded" },
+  change_detection = { notify = false },
+})
